986670be7ae9c8b5f00e598f6d627232faa1c182
Kent-Andre Mardal <kent-and@simula.no>
2023-01-18 16:15:06 +0100

    update

---
+++ chapters/elliptic/code/poisson.py
@@ -0,0 +1,30 @@
+
+from dolfin import *
+
+# Create mesh and define function space
+mesh = UnitSquareMesh(32, 32)
+V = FunctionSpace(mesh, "Lagrange", 1)
+
+# Define Dirichlet boundary (x = 0 or x = 1)
+def boundary(x):
+    return x[0] < DOLFIN_EPS or x[0] > 1.0 - DOLFIN_EPS
+
+# Define boundary condition
+u0 = Constant(0.0)
+bc = DirichletBC(V, u0, boundary)
+
+# Define variational problem
+u = TrialFunction(V)
+v = TestFunction(V)
+f = Constant(1)
+g = Expression("x[0]")
+a = inner(grad(u), grad(v))*dx
+L = f*v*dx + g*v*ds
+
+# Compute solution
+u = Function(V)
+solve(a == L, u, bc)
+
+# Save solution in VTK format
+file = File("poisson.pvd")
+file << u
